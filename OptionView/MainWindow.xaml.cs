e06787bf1c29102dd57180c8e8548e77e2f46b82
Scott Andrews <mscott.andrews@gmail.com>
2019-03-08 17:42:36 -0600

    Initial migration from UI POC

--- OptionView/MainWindow.xaml.cs
+++ OptionView/MainWindow.xaml.cs
@@ -21,6 +21,12 @@ namespace OptionView
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int left = 10;
+        private int top = 10;
+        private bool nextColor = true;
+
+
+
         public MainWindow()
         {
             //DataLoader.Load("all transactions.csv");
@@ -45,6 +51,68 @@ namespace OptionView
             App.CloseConnection();
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            left += 10;
+            top += 10;
+            nextColor = !nextColor;
+            //CreateTile(nextColor, "NDVA", left, top, "blah");
 
+            Canvas canvas = MainCanvas;
+            Tiles t = new Tiles();
+            t.CreateTile(this, MainCanvas, nextColor, "NDVA", left, top, "blah");
+
+        }
+
+        //private void CreateTile(bool green, string symbol, int left, int top, string details)
+        //{
+        //    //<ContentControl Canvas.Top = "10" Canvas.Left = "10" Template = "{StaticResource DesignerItemTemplate}" >
+        //    //  <Canvas Style = "{DynamicResource TileCanvas}" >
+        //    //     < TextBlock Text = "Symbol" Style = "{DynamicResource SymbolHeader}" />
+        //    //     < TextBlock Text = "Text" Canvas.Top = "19" Style = "{DynamicResource SymbolDetails}" />
+        //    //     < TextBlock Text = "Text2" Canvas.Top = "38" Style = "{DynamicResource SymbolDetails}" />
+        //    //  </ Canvas >
+        //    //</ ContentControl > 
+
+        //    ContentControl cc = new ContentControl();
+        //    if (green)
+        //        cc.Template = (ControlTemplate)this.Resources["GreenTileTemplate"];
+        //    else
+        //        cc.Template = (ControlTemplate)this.Resources["RedTileTemplate"];
+
+
+        //    Canvas tileCanvas = new Canvas();
+        //    tileCanvas.Style = (Style)this.Resources["TileCanvas"];
+        //    cc.Content = tileCanvas;
+
+        //    TextBlock txtSymbol = new TextBlock()
+        //    {
+        //        Text = symbol,
+        //        Style = (Style)this.Resources["SymbolHeader"]
+        //    };
+        //    tileCanvas.Children.Add(txtSymbol);
+
+        //    TextBlock txtDetail1 = new TextBlock()
+        //    {
+        //        Text = details,
+        //        Style = (Style)this.Resources["SymbolDetails"],
+        //    };
+        //    Canvas.SetTop(txtDetail1, 14);
+        //    tileCanvas.Children.Add(txtDetail1);
+
+        //    TextBlock txtDetail2 = new TextBlock()
+        //    {
+        //        Text = "tbd",
+        //        Style = (Style)this.Resources["SymbolDetails"]
+        //    };
+        //    Canvas.SetTop(txtDetail2, 28);
+        //    tileCanvas.Children.Add(txtDetail2);
+
+
+        //    Canvas.SetLeft(cc, left);
+        //    Canvas.SetTop(cc, top);
+
+        //    MainCanvas.Children.Add(cc);
+        //}
     }
 }
---
+++ OptionView/MoveTile.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace OptionView
+{
+
+    public class MoveTile : Thumb
+    {
+        public MoveTile()
+        {
+            DragDelta += new DragDeltaEventHandler(this.MoveThumb_DragDelta);
+        }
+
+        private void MoveThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            Control tile = this.DataContext as Control;
+
+            if (tile != null)
+            {
+                double left = Canvas.GetLeft(tile);
+                double top = Canvas.GetTop(tile);
+                double tileWidth = tile.ActualWidth;
+                double tileHeight = tile.ActualHeight;
+                double canvasWidth = ((Canvas)tile.Parent).ActualWidth;
+                double canvasHeight = ((Canvas)tile.Parent).ActualHeight;
+
+                double newLeft = left + e.HorizontalChange;
+                newLeft = newLeft < 0 ? 0 : newLeft;
+                newLeft = (newLeft + tileWidth) > (canvasWidth) ? canvasWidth - tileWidth : newLeft;
+                double newTop = top + e.VerticalChange;
+                newTop = newTop < 0 ? 0 : newTop;
+                newTop = (newTop + tileHeight) > (canvasHeight) ? canvasHeight - tileHeight : newTop;
+                Canvas.SetLeft(tile, newLeft);
+                Canvas.SetTop(tile, newTop < 0 ? 0 : newTop);
+            }
+        }
+    }
+}
