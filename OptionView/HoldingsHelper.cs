fda6269f03c85d4f36b319c03391e48ec05f17e5
Scott Andrews <mscott.andrews@gmail.com>
2019-03-11 18:17:28 -0500

    Added CurrentHoldings method

--- OptionView/HoldingsHelper.cs
+++ OptionView/HoldingsHelper.cs
@@ -16,6 +16,73 @@ namespace OptionView
         {
             return true;
         }
+
+
+
+
+        public static Portfolio CurrentHoldings()
+        {
+            Portfolio portfolio = new Portfolio();
+
+            try
+            {
+                // establish connection
+                App.OpenConnection();
+
+                string sql = "SELECT * FROM transgroup AS tg LEFT JOIN";
+                sql += " (SELECT transgroupid, sum(amount) as Cost from transactions GROUP BY  transgroupid) AS t ON tg.id = t.transgroupid";
+                sql += " WHERE tg.Open = 1";
+
+                SQLiteCommand cmd = new SQLiteCommand(sql, App.ConnStr);
+                SQLiteDataReader readerGroup = cmd.ExecuteReader();
+                while (readerGroup.Read())
+                {
+                    Underlying underlying = new Underlying();
+
+                    underlying.Symbol = readerGroup["Symbol"].ToString();
+                    underlying.TransactionGroup = Convert.ToInt32(readerGroup["ID"]); // readerGroup
+                    underlying.Cost = Convert.ToDecimal(readerGroup["Cost"]);
+                    if (readerGroup["X"] != DBNull.Value) underlying.X = Convert.ToInt32(readerGroup["X"]);
+                    if (readerGroup["Y"] != DBNull.Value) underlying.X = Convert.ToInt32(readerGroup["Y"]);
+                    if (readerGroup["Comments"] != DBNull.Value) underlying.Comments = readerGroup["Comments"].ToString();
+
+                    // step thru open holdings
+                    sql = "SELECT * FROM (SELECT symbol, transgroupid, type, datetime(expiredate) AS ExpireDate, strike, sum(quantity) AS total, sum(amount) as amount FROM transactions";
+                    sql += " GROUP BY symbol, type, expiredate, strike) WHERE (transgroupid = @gr) and (total <> 0)";
+                    cmd = new SQLiteCommand(sql, App.ConnStr);
+                    cmd.Parameters.AddWithValue("gr", underlying.TransactionGroup);
+
+                    SQLiteDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        //decimal strike = reader["strike"].ToString();
+                        decimal strike = 0m;
+                        if (reader["Strike"] != DBNull.Value) strike = Convert.ToDecimal(reader["Strike"]);
+                        decimal quantity = 0.0m;
+                        if (reader["Total"] != DBNull.Value) quantity = Convert.ToDecimal(reader["Total"]);
+                        decimal amount = 0.0m;
+                        if (reader["Amount"] != DBNull.Value) amount = Convert.ToDecimal(reader["Amount"]);
+                        DateTime expDate = DateTime.MinValue;
+                        if (reader["ExpireDate"] != DBNull.Value) expDate = Convert.ToDateTime(reader["ExpireDate"].ToString());
+
+                        underlying.Holdings.AddTransaction(reader["symbol"].ToString(), reader["type"].ToString(), expDate, strike, quantity, amount, 0, "", 0);
+                    }
+
+                    portfolio.Add(underlying);
+
+                }  // end of transaction group loop
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("CurrentHoldings: " + ex.Message );
+            }
+
+            return portfolio;
+        }
+
+
+
+
         public static void UpdateNewTransactions()
         {
             string symbol = "";
@@ -52,8 +119,11 @@ namespace OptionView
 
                     while (dt.Rows.Count > 0)
                     {
+                        int numOfRows = dt.Rows.Count;
                         string t = dt.Rows[0]["TransTime"].ToString(); // maintain sqlite date format as a string 
                         ProcessTransactionChain(symbol, dt, t);
+
+                        if (dt.Rows.Count == numOfRows) break;  // nothing happened, so avoid endless loop
                     }
 
 
@@ -61,7 +131,7 @@ namespace OptionView
             }
             catch (Exception ex)
             {
-                Debug.WriteLine("QUERY: " + ex.Message + "(" + symbol + ")");
+                Debug.WriteLine("UpdateNewTransactions: " + ex.Message + "(" + symbol + ")");
             }
 
  
