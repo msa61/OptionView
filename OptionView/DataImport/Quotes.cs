72075177ecdf4f99611b1ad83f6ad5d398d56655
Scott Andrews <mscott.andrews@gmail.com>
2021-07-06 15:35:17 -0500

    Added "Quotes" class with required HtmlAgilityPack

---
+++ OptionView/DataImport/Quotes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace OptionView
+{
+    class Quotes
+    {
+        public static Decimal Get(string symbol)
+        {
+            Decimal quote = 0;
+
+            try
+            {
+                string url = "https://finance.yahoo.com/quote/" + symbol;
+                HtmlWeb web = new HtmlWeb();
+                HtmlDocument doc = web.Load(url);
+
+                var node = doc.GetElementbyId("quote-header-info");
+                string value = node.ChildNodes[2].ChildNodes[0].ChildNodes[0].ChildNodes[0].InnerText;
+
+                quote = Convert.ToDecimal(value);
+            }
+            catch (Exception e)
+            {
+
+            }
+            return quote;
+        }
+
+    }
+
+}
